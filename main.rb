88df7796e058337323516bbe4c678787f66ed0bb
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-28 10:57:17 -0700

    Add main.rb and pseudocode

---
+++ main.rb
@@ -0,0 +1,32 @@
+## main.rb
+# get names + chomp
+# randomizer for who goes first?
+# create player objects with name and starting order
+# while loop
+# check if player has enough pieces/ win
+# place piece down
+# continue or stop
+# print out who wins
+
+
+##Create a player class
+# player name
+# place an x/o at a certain position function gets
+# keeps track of where you've place pieces
+# number of starting pieces? 5 for player one 4 for player two
+
+
+##Board class?
+#should check wins here maybe?
+#check if position has been taken
+#Board Print out
+# _________________
+#|     |     |     |
+#|  0  |  1  |  2  |
+#|_____|_____|_____|
+#|     |     |     |
+#|  3  |  4  |  5  |
+#|_____|_____|_____|
+#|     |     |     |
+#|  6  |  7  |  8  |
+#|_____|_____|_____|
