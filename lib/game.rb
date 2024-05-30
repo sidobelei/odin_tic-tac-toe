66b486de5bb2194ad372768970de8283d160117b
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-30 10:22:05 -0700

    Refactor game.rb to remove leftover comments

--- lib/game.rb
+++ lib/game.rb
@@ -30,22 +30,12 @@ class Game
         return game_over
     end
     
-    #in a row function 
-    #take placed pieces from player and name
-    #see if they fit a three in a row pattern
-    #if so change game_over to true and winner to user name
-    #else do nothing
-    #return game_over
-    
     def full_board?(player_one_pieces, player_two_pieces)
         if (player_one_pieces + player_two_pieces).length >= ALLOWED_PIECES
             @game_over = true
         end
         return @game_over
     end
-    #board is full function
-    #take placed pieces from both player see if equals 9
-    #if so game_over to true
 
     def declare_winner
         if winner
@@ -54,7 +44,4 @@ class Game
             puts "It's a tie!"
         end
     end
-    #declare winner function
-    #print out winner 
-
 end
